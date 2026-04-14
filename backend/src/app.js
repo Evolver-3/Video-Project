ae1da3c89f8d4f11d4dbafce006869159f63fd9b
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-14 23:59:13 +0530

    added cookieParser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,30 @@
 import express from 'express'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app=express()
 
-app.use(express.json())
+app.use(cors({
+  origin:process.env.CORS || 'http://localhost:5173',
+  credentials:true
+}))
+
+app.use(express.json({  
+    limit:"16kb"
+}))
+
+app.use(express.urlencoded({
+  extended:true,
+  limit:"16kb"
+}))
+
+app.use(cookieParser())
 
 
 import {router} from './routes/user.routes.js'
 app.use("/api/v1/users",router)
 
+import { videoRouter } from './routes/video.routes.js'
+app.use("/api/v1/video",videoRouter)
 
 export {app}
