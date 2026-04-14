e1a44dff69a2e175547e9ea7f509a15a5d3fb927
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-14 23:53:43 +0530

    fix the middleware typo error

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -4,8 +4,9 @@ import { asyncHandler } from '../utils/asyncHandler.js'
 import {ApiError} from '../utils/ApiError.js'
 
 
-export const verifyJWt=asyncHandler(async(req,res,next)=>{
+export const verifyJWT=asyncHandler(async(req,res,next)=>{
   try{
+
     const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
 
     if(!token){
@@ -13,6 +14,8 @@ export const verifyJWt=asyncHandler(async(req,res,next)=>{
     }
 
     const decodedToken=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+    
 
     const user=await User.findById(decodedToken?._id).select("-password -refreshToken")
 
