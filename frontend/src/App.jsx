7e4126ecd71c7c1a40a45b8e976a0e8607cb0e14
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-15 23:57:05 +0530

    fixed the wrong import error

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import {RouterProvider} from 'react-router-dom'
+import { router } from './app.routes.jsx'
+import { AuthProvider } from './features/auth/auth.context.jsx'
+import { VideoProvider } from './features/video/video.context.jsx'
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <VideoProvider>
+        <RouterProvider router={router}/>
+      </VideoProvider>
+    </AuthProvider>
+  )
+}
+
+export default App
