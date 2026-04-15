03c9c58a5132b80e07f3cafb06782d190d3ad6a1
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-15 23:56:06 +0530

    add useVideo hook for video management functionalities

---
+++ frontend/src/features/video/hooks/useVideo.js
@@ -0,0 +1,130 @@
+import { useContext } from "react";
+import { VideoPost, VideoGetAll, VideoById, PublishFlag, LikeFlag } from "../services/video.api";
+import { VideoContext } from "../video.context.jsx";
+import {useParams} from 'react-router-dom'
+
+export const useVideo=()=>{
+
+  const context=useContext(VideoContext)
+
+  const {videoId}=useParams()
+
+    if(!context){
+    throw new Error("does this even exist!!")
+  }
+
+  const {videoData,setVideoData,loading,setLoading,errorMessage,setErrorMessage}=context
+
+   const handleVideoUpload=async({title,description,videoUrl})=>{
+
+    setLoading(true)
+
+    try {
+
+      const res=await VideoPost({title,description,videoUrl})
+
+      console.log(res)
+
+      setVideoData(res)
+
+      return true
+      
+    } catch (error) {
+      setErrorMessage(error)
+      console.log("error in uploading video:",error)
+      return false
+      
+    }finally{
+      setLoading(false)
+    }
+
+   }
+
+   const handleVideoGetAll=async()=>{
+
+    setLoading(true)
+
+    try{
+
+      const res=await VideoGetAll()
+      setVideoData(res)
+      console.log(res)
+
+    }catch(error){
+
+      setErrorMessage(error)
+      console.log("Error getting videos:",error)
+      return false
+
+    }finally{
+      setLoading(false)
+    }
+
+   }
+
+   const handleVideoGetById=async(videoId)=>{
+
+     setLoading(true)
+
+    try{
+      const res =await VideoById(videoId)
+      console.log(res)
+      setVideoData(res)
+
+    }catch(error){
+
+      setErrorMessage(error)
+
+      console.log("Error getting videos by Id:",error)
+      return false
+    }finally{
+      setLoading(false)
+    }
+
+   }
+
+   const handlePublish=async(videoId)=>{
+
+     setLoading(true)
+
+    try{
+      const res=await PublishFlag(videoId)
+      
+      setVideoData(res)
+
+      console.log(res)
+
+    }catch(error){
+
+      setErrorMessage(error)
+      console.log("Error changing Visibility state:",error)
+      return false
+    }finally{
+      setLoading(false)
+    }
+
+   }
+   
+   const handleLike=async(id=videoId)=>{
+
+     setLoading(true)
+
+    try{
+      const res=await LikeFlag(id=videoId)
+
+      setVideoData(res)
+      console.log(res)
+
+    }catch(error){
+
+      setErrorMessage(error)
+      console.log("Error toggling likes",error)
+      return false
+    }finally{
+      setLoading(false)
+    }
+
+   }
+
+   return {handleVideoUpload,handleVideoGetAll,handleVideoGetById,handlePublish,handleLike,videoData,loading,errorMessage}
+}
