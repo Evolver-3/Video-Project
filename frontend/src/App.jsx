5cd074288ff2a3d15f82243812fde106f4ae75f4
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-17 20:35:44 +0530

    feat: integrate ThemeProvider in App component and update routing for OwnerPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import {RouterProvider} from 'react-router-dom'
 import { router } from './app.routes.jsx'
 import { AuthProvider } from './features/auth/auth.context.jsx'
 import { VideoProvider } from './features/video/video.context.jsx'
+import { ThemeProvider } from './features/theme.context.jsx'
 
 const App = () => {
   return (
     <AuthProvider>
       <VideoProvider>
-        <RouterProvider router={router}/>
+        <ThemeProvider>
+          <RouterProvider router={router}/>
+        </ThemeProvider>
       </VideoProvider>
     </AuthProvider>
   )
--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -4,8 +4,9 @@ import HomePage from "./features/video/pages/HomePage.jsx";
 import RegisterPage from "./features/auth/pages/RegisterPage.jsx";
 import LoginPage from "./features/auth/pages/LoginPage.jsx";
 import VideoId from "./features/video/pages/VideoId.jsx";
-import VideoUpload from "./features/video/pages/VideoUpload.jsx";
+import VideoUpload from "./features/video/pages/uploadData/VideoUpload.jsx";
 import Protected from "./features/auth/pages/Protected.jsx";
+import OwnerPage from "./features/video/pages/ownerPage/OwnerPage.jsx";
 
 
 export const router=createBrowserRouter([
@@ -28,5 +29,9 @@ export const router=createBrowserRouter([
   {
     path:"/upload",
     element:<Protected><VideoUpload/></Protected>
+  },
+  {
+    path:"/user/:userId",
+    element:<OwnerPage/>
   }
 ])
