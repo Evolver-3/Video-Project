e455e167b6fa92f93790c2c781b3bc2d12c6fea3
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-17 20:32:40 +0530

    add ownerData state and update VideoContext.Provider value in VideoProvider

--- frontend/src/features/video/video.context.jsx
+++ frontend/src/features/video/video.context.jsx
@@ -6,11 +6,12 @@ export const VideoProvider=({children})=>{
 
   const [videoData,setVideoData]=useState([])
   const [videoDataById,setVideoDataById]=useState(null)
+  const [ownerData,setOwnerData]=useState([])
   const [loading,setLoading]=useState(false)
   const [errorMessage,setErrorMessage]=useState(null)
 
   return(
-    <VideoContext.Provider value={{videoData,setVideoData,videoDataById,setVideoDataById,loading,setLoading,errorMessage,setErrorMessage}}>
+    <VideoContext.Provider value={{videoData,setVideoData,videoDataById,setVideoDataById,loading,setLoading,errorMessage,setErrorMessage,ownerData,setOwnerData}}>
       {children}
     </VideoContext.Provider>
   )
