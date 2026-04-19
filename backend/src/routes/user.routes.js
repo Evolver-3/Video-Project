71ec9fcfc1a3e1e146a8515b8bfd384d2a45ef99
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-19 23:30:23 +0530

    feat: add logout route to user routes and enhance video routes with comments

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginController, refreshAccessToken, registerController,getUserProfile } from "../controllers/user.controller.js";
+import { loginController, refreshAccessToken, registerController,getUserProfile, logoutUser} from "../controllers/user.controller.js";
 import upload from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -9,6 +9,8 @@ const router=Router()
 router.route("/register").post(upload.single("avatar"),registerController)
 
 router.route("/login").post(loginController)
+
+router.route("/logout").post(verifyJWT,logoutUser)
 
 router.route("/refreshToken").post(refreshAccessToken)
 
--- backend/src/routes/video.routes.js
+++ backend/src/routes/video.routes.js
@@ -5,17 +5,27 @@ import { verifyJWT } from '../middlewares/auth.middleware.js'
 
 const videoRouter=Router()
 
+// route for uploading videos need verification
+
 videoRouter.route("/uploadVideo").post(verifyJWT,upload.single("videoUrl"),videoController)
+
+//getting all videos for Home page
 
 videoRouter.route("/allVideos").get(getAllVideos)
 
+//getting all videos of a particular user with his details
+
 videoRouter.route("/user/:userId").get(getAllByOwner)
+
+//getting a details of a video by it's id
 
 videoRouter.route("/:videoId").get(getVideoById)
 
+//changing the visibility of a video either private or public
+
 videoRouter.route("/:videoId/publish").patch(verifyJWT,changePublishStatus)
 
-
+//router for like button
 
 videoRouter.route("/:videoId/like").post(verifyJWT,videoLikeCount)
 
