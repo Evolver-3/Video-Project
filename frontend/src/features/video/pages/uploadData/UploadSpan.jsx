10f5dc648c71b0934a5e01927b337c2951a11b01
Ashu Singh <Ashusingh2201@gmail.com>
2026-04-20 23:58:33 +0530

    feat: simplify UploadSpan label styling by removing gradient animation

--- frontend/src/features/video/pages/uploadData/UploadSpan.jsx
+++ frontend/src/features/video/pages/uploadData/UploadSpan.jsx
@@ -10,7 +10,7 @@ const UploadSpan = ({videoUpload,videoRef}) => {
 
      <div className=' text-lg font-semibold  rounded-3xl px-10 py-1 relative z-50 overflow-hidden shadow-inset hover:scale-98 active:scale-100 transition-all duration-300 bg-rose-100'>
 
-      <label className=' bg-[linear-gradient(30deg,red_10%,blue_10%,purple_10%,green_63%)] bg-size-[200%_100%] bg-position-[-200%_0] bg-clip-text text-transparent animate-[shimmer_2s_linear_infinite] cursor-pointer'>Select Video
+      <label className=' text-neutral-700 cursor-pointer'>Select Video
       
       <input hidden type="file" ref={videoRef}
       accept='video/*' />
